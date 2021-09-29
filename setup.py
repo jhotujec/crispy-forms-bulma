df1c514329b2cb4a5948ddc194b58462ea8ad907
Francis Secada <francis.secada@gmail.com>
2021-09-28 22:41:14 -0400

    requirements update; pulled codebase for compliance with Django 3.0

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 from setuptools import find_packages, setup
+import os
 
 setup(
-    name='django-crispy-bulma',
-    version='0.3',
-    description='Django application to add \'django-crispy-forms\' layout objects for Bulma.io',
-    long_description=open('README.md').read(),
+    name="django-crispy-bulma",
+    version="0.3",
+    description="Django application to add 'django-crispy-forms' layout objects for Bulma.io",
+    long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    author='Python Discord',
-    author_email='staff@pythondiscord.com',
-    url='https://github.com/python-discord/django-crispy-bulma',
-    license='MIT',
+    author="Python Discord",
+    author_email="staff@pythondiscord.com",
+    url="https://github.com/python-discord/django-crispy-bulma",
+    license="MIT",
     packages=find_packages(),
     classifiers=[
         "Development Status :: 3 - Alpha",
@@ -28,24 +29,11 @@ setup(
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
-    install_requires=[
-        'Django>=2.2',
-        'django-crispy-forms~=1.9.0'
-    ],
     extras_require={
-        "dev": [
-            "flake8~=3.7",
-            "flake8-bugbear~=19.8",
-            "flake8-import-order~=0.18",
-            "flake8-tidy-imports~=2.0",
-            "flake8-todo~=0.7",
-            "flake8-string-format~=0.2",
-            "pdoc~=0.3",
-            "pre-commit~=1.18",
-            "PyGithub~=1.43",
-            "wheel~=0.33",
-        ]
+        "dev": open(
+            os.path.join(os.path.dirname(__file__), "dev_requirements.txt")
+        ).read(),
     },
     include_package_data=True,
-    zip_safe=False
+    zip_safe=False,
 )
